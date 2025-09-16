23ab0e7a8be06f0a1e135b1168a8ab3711c48be4
unnza <amzorromanov@yandex.ru>
2025-09-16 19:29:11 +0300

    Добавил код для ШИМ-диммирования светодиода

---
+++ get-led/pwm-led.py
@@ -0,0 +1,15 @@
+import RPi.GPIO as gp
+import time
+gp.setmode(gp.BCM)
+led = 26 
+gp.setup(led, gp.OUT)
+pwm = gp.PWM(led, 200)
+duty = 0.0
+pwm.start(duty)
+while True:
+    pwm.ChangeDutyCycle(duty)
+    time.sleep(0.05)
+
+    duty += 1.0
+    if duty > 100.0:
+        duty = 0.0
