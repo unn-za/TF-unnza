273c2f57ba0a7c31fc315112436a6ccdf2020d5c
unnza <amzorromanov@yandex.ru>
2025-09-30 20:07:05 +0300

    Добавил кривой класс (не работает)

---
+++ get-dac/r2r_dac.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as gp
+
+class R2R_DAC:
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
+        self.gpio_bits = gpio_bits
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        gp.setmode(gp.BCM)
+        gp.setup(self.gpio_bits, gp.OUT, initial = 0)
+    def definit(self):
+        gp.output(self.gpio_bits, 0)
+        gp.cleanup()
+    def set_number(self, number):
+        gp.output(self.gpio_bits, [int(element) for element in bin(number)[2:].zfill(8)])
+    def set_voltage(self, voltage):
+        set_number(self, voltage)
+
+if __name__ == "__main__":
+    dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
+    try:    
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+            
+            except ValueError:
+                print("Вы ввели не число. Попробуйте еще раз\n")
+    finally:
+        dac.deinit()
+
+
