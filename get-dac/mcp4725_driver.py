637c6d9052c3ee55cec3ad937ffda2a937b4b802
unnza <amzorromanov@yandex.ru>
2025-10-07 21:25:38 +0300

    Добавил код для MCP4725

---
+++ get-dac/mcp4725_driver.py
@@ -0,0 +1,48 @@
+import smbus
+
+class MCP4725:
+    def __init__(self, dynamic_range, address=0x61, verbose = True):
+        self.bus = smbus.SMBus(1)
+        
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+        
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
+
+    def deinit(self):
+        self.bus.close()
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("На вход ЦАП можно подавать только целые числа")
+
+        if not (0 <= number <= 4095):
+            print("Число выходит за разраядность MCP4752 (12 бит)")
+
+        first_byte = self.wm | self.pds | number >> 8
+        second_byte = number & 0xFF
+        self.bus.write_byte_data(0x61, first_byte, second_byte)
+        if self.verbose:
+            print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
+    def set_voltage(self, voltage):
+        if voltage < 0 or voltage > self.dynamic_range:
+            voltage = 0
+            print("tfkyadoin?")
+        number = int((voltage / self.dynamic_range)*4095)
+        self.set_number(number)
+        
+
+if __name__ == "__main__":
+    dac = MCP4725(5.11)
+    try:
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в Вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте ещё раз\n")
+
+    finally:
+        dac.deinit()
