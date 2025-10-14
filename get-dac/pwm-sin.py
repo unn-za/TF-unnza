3bab5b6885bb24389382ecb363fec72cb994b94f
unnza <amzorromanov@yandex.ru>
2025-10-14 19:07:42 +0300

    Добавили ШИМ синус

---
+++ get-dac/pwm-sin.py
@@ -0,0 +1,14 @@
+import pwm_dac as pm
+import signal_generator as sg
+import time
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+if __name__ == "__main__":
+    dac = pm.PWM_DAC(12, 500, 3.290, True)
+    while True:
+        try:
+            sg.wait_for_sampling_period(sampling_frequency)
+            dac.set_voltage(amplitude*sg.get_sin_wave_amplitude(signal_frequency, time.time()))
+        finally:
+            pass 
