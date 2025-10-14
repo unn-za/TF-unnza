f6e0ce837155bb3a2899f00cffd9cf5a160bc7cd
unnza <amzorromanov@yandex.ru>
2025-10-14 19:54:33 +0300

    Добавил синус через I2C

---
+++ get-dac/mcp4725-sin.py
@@ -0,0 +1,14 @@
+import mcp4725_driver as mc
+import signal_generator as sg
+import time
+amplitude = 3.2
+signal_frequency = 10
+sampling_frequency = 1000
+if __name__ == "__main__":
+    dac = mc.MCP4725(5.11, 0x61, True)
+    while True:
+        try:
+            sg.wait_for_sampling_period(sampling_frequency)
+            dac.set_voltage(amplitude*sg.get_sin_wave_amplitude(signal_frequency, time.time()))
+        finally:
+            pass 
--- get-dac/signal_generator.py
+++ get-dac/signal_generator.py
@@ -4,4 +4,5 @@ def get_sin_wave_amplitude(freq, time):
     return((1+np.sin(2*np.pi*freq*time))/2)
 def wait_for_sampling_period(sampling_frequency):
     time.sleep(1/sampling_frequency)
-
+# def get_triangle_wave_amplitude(freq, time):
+    
